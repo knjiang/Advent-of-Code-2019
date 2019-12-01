5fcbe94cc91daf06667fbbe23a6f27c3d35c7733
Ken Jiang <jiangk@bxscience.edu>
2019-12-01 13:32:57 -0500

    day 1 completed

--- day1.py
+++ day1.py
@@ -3,7 +3,18 @@ import math
 def main():
     with open("input_day1.txt") as f:
         lst = [int(x) for x in f.read().split()]
-    print(sum([fuel_req(x) for x in lst]))
+    print("part 1 sum : " + str(sum([fuel_req(x) for x in lst])))
+    print("part 2 sum : " + str(sum([rec_fuel_req(x) for x in lst])))
+"""
+Part 2 Solution
+"""
+def rec_fuel_req(mass):
+    if fuel_req(mass) <= 0:
+        return 0
+    return rec_fuel_req(fuel_req(mass)) + fuel_req(mass)
+"""
+Part 1 Solution 
+"""
 def fuel_req(mass):
     return (math.floor(mass / 3) - 2)
 if __name__ == "__main__":
